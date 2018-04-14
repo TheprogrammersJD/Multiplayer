8d09201165a4dcf7ecf5f229568fa7115c527bff
TheprogrammersJD <solarisfree101@gmail.com>
2018-04-14 18:03:51 -0500

    Update index.js

--- index.js
+++ index.js
@@ -8,11 +8,29 @@ app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
+var clickx = [];
+var clicky = [];
+
+app.get('/', function(req, res){
+res.sendFile(__dirname + '/index.html');
+});
+
 io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
+  console.log('a user connected');
+  for(var n = 0; n < clickx.length; n++){
+		socket.emit('click', clickx[n], clicky[n]);
+  }
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+  socket.on('click', function(x, y){
+    console.log('Somebody clicked at: ' + x + ", " + y);
+	clickx.push(x);
+	clicky.push(y);
+	socket.broadcast.emit('click', x, y);
   });
 });
+
 
 http.listen(port, function(){
   console.log('listening on *:' + port);
